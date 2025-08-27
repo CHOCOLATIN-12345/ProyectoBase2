f3de02ec355725d8a54bb1931794627eea0bf094
CHOCOLATIN-12345 <vict.sandovalc@duocuc.cl>
2025-08-27 11:17:57 -0400

    creamos las operaciones

--- app/src/main/java/com/example/proyectobase2/MainActivity4.kt
+++ app/src/main/java/com/example/proyectobase2/MainActivity4.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyectobase2.utils.OpMatematicas
 
 class MainActivity4 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +19,16 @@ class MainActivity4 : AppCompatActivity() {
         val edNumero2:EditText = findViewById(R.id.ed_numero2)
         val txtMensaje:TextView = findViewById(R.id.txt_mensaje)
         val btnCalcular:Button = findViewById(R.id.boton_calcular)
+
+        btnCalcular.setOnClickListener {
+            var calN1:Int = edNumero1.text.toString().toIntOrNull()?:0
+            var calN2:Int = edNumero2.text.toString().toIntOrNull()?:0
+
+            var resultadoOP: Int = OpMatematicas.resta(calN1,calN2)
+
+            txtMensaje.text = resultadoOP.toString()
+
+        }
 
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
---
+++ app/src/main/java/com/example/proyectobase2/utils/OpMatematicas.kt
@@ -0,0 +1,21 @@
+package com.example.proyectobase2.utils
+
+object OpMatematicas {
+
+    /*Funcion sumar recibe 2 parametros y retorna un numero entero numerico*/
+    fun sumar(n1: Int, n2:Int ): Int{
+        return  n1+n2
+    }
+
+    fun resta(n1: Int, n2: Int): Int{
+        return n1-n2
+    }
+
+    fun dividir(n1: Int, n2: Int): Int {
+        return n1/n2
+    }
+
+    fun multiplicar(n1: Int, n2: Int): Int{
+        return n1*n2
+    }
+}
