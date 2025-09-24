d28f248b3814a4d2d224ae66e21b12fe1a4b67d4
CHOCOLATIN-12345 <vict.sandovalc@duocuc.cl>
2025-09-24 10:45:40 -0300

    Se dieron permisos de internet y las dependencias

---
+++ app/src/main/java/com/example/proyectobase2/MainActivity6.kt
@@ -0,0 +1,27 @@
+package com.example.proyectobase2
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main6)
+
+        val spRegion:Spinner=findViewById(R.id.sp_region)
+        val spComuna:Spinner=findViewById(R.id.sp_comuna)
+        val btnAlgo:Button=findViewById(R.id.btn_algo)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
